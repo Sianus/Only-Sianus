c1fd63ac451275df67565733916f33daad4623b3
Defi <defi622@naver.com>
2022-01-21 13:25:12 +0900

    Check the link

---
+++ Cpp/Solved.cpp/bronze5/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    while(n*m ==0 ) cin >> n >> m;
+    cout << n/m << endl;
+    cout << n%m << endl;
+
+    return 0;
+}
+
+//https://blog.naver.com/PostView.nhn?blogId=dlscjs8646&logNo=222152061070
